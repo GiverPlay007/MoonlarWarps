dc6af581f7376085e80de3e711d55e108b089bec
GiverPlay007 <play.giver.007@gmail.com>
2021-10-26 21:53:02 -0300

    Initialize project

---
+++ src/main/java/net/moonlar/warps/MoonlarWarps.java
@@ -0,0 +1,16 @@
+package net.moonlar.warps;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MoonlarWarps extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
