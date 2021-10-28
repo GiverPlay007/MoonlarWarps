0bb6bab7dc38370b5e4fd86a070c5ffb6707f678
GiverPlay007 <play.giver.007@gmail.com>
2021-10-27 21:57:20 -0300

    Hook Essentials

--- src/main/java/net/moonlar/warps/MoonlarWarps.java
+++ src/main/java/net/moonlar/warps/MoonlarWarps.java
@@ -1,16 +1,31 @@
 package net.moonlar.warps;
 
+import net.moonlar.warps.providers.EssentialsWarpProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MoonlarWarps extends JavaPlugin {
 
+  private WarpProvider warpProvider;
+
   @Override
   public void onEnable() {
+    try {
+      warpProvider = new EssentialsWarpProvider();
+    } catch (UnsupportedOperationException e) {
+      e.printStackTrace();
+      getServer().getPluginManager().disablePlugin(this);
+      return;
+    }
 
+    getLogger().fine("Using Essentials as warp provider");
   }
 
   @Override
   public void onDisable() {
+    warpProvider = null;
+  }
 
+  public WarpProvider getWarpProvider() {
+    return warpProvider;
   }
 }
